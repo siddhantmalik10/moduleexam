31bd146eef05b9f97682fd1e3732253b074fcdf9
Tusahr <tusharp0911@gmail.com>
2023-05-17 12:05:24 +0530

    Uploading to git repository

---
+++ db.sql
@@ -0,0 +1,35 @@
+
+
+
+
+create table Candidate(
+c_name varchar(50),
+mobile int,
+c_email varchar(50),
+qualification varchar(50)
+);
+
+
+
+create table Job_offerings(
+company_name varchar(50),
+job_title varchar(50),
+req_qualification varchar(50),
+job_desc varchar(150)
+);
+
+create table Company(
+company_name varchar(50),
+location varchar(50),
+email varchar(50),
+about_us varchar(50)
+);
+
+
+
+
+
+
+
+
+
